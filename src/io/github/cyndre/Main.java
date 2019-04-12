5d9e2559c6f7149d0629da1b48a3e53a69a64c0e
cyndre <34636882+cyndre@users.noreply.github.com>
2019-04-11 21:26:05 -0500

    Update Main.java
    Clean up class imports

--- src/io/github/cyndre/Main.java
+++ src/io/github/cyndre/Main.java
@@ -1,6 +1,10 @@
 package io.github.cyndre;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import org.json.simple.JSONObject;
