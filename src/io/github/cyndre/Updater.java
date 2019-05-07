5e38b918997116f7b470f7fdd91680de2ffdd4ab
ericmutton <64441308+ericmutton@users.noreply.github.com>
2019-05-06 20:58:58 -0500

    Split Main.java
    Main method is now split into a driver and a runner. Includes better argument handling, logic, and variable incapsulation.

---
+++ src/io/github/cyndre/Updater.java
@@ -0,0 +1,147 @@
+package io.github.cyndre;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.json.simple.JSONArray;
+import org.apache.commons.io.FileUtils;
+
+public class Updater
+{
+    private static String release;
+    private static String filepath;
+    private static String versionId;
+    private static JSONObject versionObj;
+
+    public Updater(String type, String path)
+    {
+        setRelease(type);
+        setFilepath(path);
+    }
+
+    public static String fetchVersion(String version)
+    {
+        JSONObject manifest = JSONFromWeb("https://launchermeta.mojang.com/mc/game/version_manifest.json");
+        JSONArray versions = (JSONArray) manifest.get("versions");
+        if (getRelease().equals("release") || getRelease().equals("snapshot"))
+        {
+            version = ((JSONObject) manifest.get("latest")).get(getRelease()).toString();
+        }
+        for (int i = 0; i <= versions.size(); i++)
+        {
+            if (i == versions.size())
+            {
+                System.out.println("Could not find version "+version+"!");
+                break;
+            }
+            setVersionObj((JSONObject) versions.get(i));
+            setVersionId(getVersionObj().get("id").toString());
+            if (getVersionId().equals(version))
+            {
+                break;
+            }
+        }
+        return getVersionId();
+    }
+
+    public static void downloadVersion()
+    {
+        try
+        {
+            String url = getVersionObj().get("url").toString();
+            setVersionObj((JSONObject) (JSONFromWeb(url)).get("downloads"));
+            setVersionObj((JSONObject) getVersionObj().get("server"));
+            URL ejf = new URL(getVersionObj().get("url").toString());
+            System.out.println("Downloading "+ getVersionId() +". . .");
+            FileUtils.copyURLToFile(ejf, new File(getFilepath() +"/server.jar"));
+            System.out.println("Downloaded "+ getVersionId() +" from "+ejf);
+        }
+        catch (IOException e)
+        {
+            System.out.println("Could not finish updating. "+e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static JSONObject JSONFromWeb(String url)
+    {
+        JSONObject json = new JSONObject();
+        JSONParser parser = new JSONParser();
+        try
+        {
+            java.io.InputStream is = new URL(url).openConnection().getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            json = (JSONObject) parser.parse(br.readLine());
+            br.close();
+        }
+        catch (ParseException | IOException e)
+        {
+            System.out.println("No updates could be retrieved. "+e.getMessage());
+            e.printStackTrace();
+        }
+        return json;
+    }
+
+    public static String localVersion()
+    {
+        String line = "[00:00:00] [Server thread/INFO]: Starting minecraft server version unknown";
+        try
+        {
+            File log = new File(getFilepath() +"/logs/latest.log");
+            BufferedReader br = new BufferedReader(new FileReader(log));
+            while (!(line = br.readLine()).contains("[Server thread/INFO]: Starting minecraft server version"));
+            br.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return line.substring(67);
+    }
+
+    public static String getRelease()
+    {
+        return release;
+    }
+
+    public static void setRelease(String release)
+    {
+        Updater.release = release;
+    }
+
+    public static String getFilepath()
+    {
+        return filepath;
+    }
+
+    public static void setFilepath(String filepath)
+    {
+        Updater.filepath = filepath;
+    }
+
+    public static String getVersionId()
+    {
+        return versionId;
+    }
+
+    public static void setVersionId(String versionId)
+    {
+        Updater.versionId = versionId;
+    }
+
+    public static JSONObject getVersionObj()
+    {
+        return versionObj;
+    }
+
+    public static void setVersionObj(JSONObject versionObj)
+    {
+        Updater.versionObj = versionObj;
+    }
+}
